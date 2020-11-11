5c100515d2f36bf450701991a42cf44f0f369508
Hayden <hberge17@gmail.com>
2020-11-10 18:26:24 -0700

    added initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeamsComponent } from '@components/teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'teams',
+    component: TeamsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
