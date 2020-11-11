e1625a45b88664227de4b78383cd01a6c7aa7a60
Hayden <hberge17@gmail.com>
2020-11-10 18:26:24 -0700

    added initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/common/header/header.component';
+import { HeaderComponent } from '../components/common/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TeamsComponent } from '@components/teams/teams.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     NgbModule
   ],
   providers: [],
